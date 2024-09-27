5cf30e92962bb29e0feaa136359e5e4e935e3b36
MrBowis <02alejo20@gmail.com>
2024-09-27 16:26:27 -0500

    Card complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { Card, CardHeader, CardBody, Divider } from "@nextui-org/react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [mensajes, setMensajes] = useState<string[]>([
+    "Feliz cumpleaños 🥰 🎂, TE AMAMOS NUNI !!! ❤️",
+    "Mamá, tu amor es el refugio más seguro que tengo. Gracias por ser mi apoyo incondicional.",
+    "Eres la razón por la que creo en la bondad del mundo. Te amo más de lo que las palabras pueden expresar.",
+    "Cada día contigo es un regalo. Gracias por ser la mejor mamá del universo.",
+    "Tu sonrisa ilumina mis días y tu sabiduría guía mis pasos. Eres una inspiración para mí.",
+    "Mamá, tu amor y sacrificio no pasan desapercibidos. Te valoro más de lo que puedes imaginar.",
+    "Eres la mujer más fuerte que conozco. Gracias por enseñarme a ser valiente y resiliente.",
+  ]);
+
+  const fechaActual = new Date().toLocaleDateString();
+
+  // Retornar un mensaje diferente dependiendo del dia de la semana
+  const message = (function () {
+    const dia = new Date().getDay();
+    if (new Date().getMonth() === 8 && new Date().getDate() === 28) {
+      return "Feliz cumpleaños 🥰 🎂, TE AMAMOS NUNI !!! ❤️";
+    }
+    switch (dia) {
+      case 0:
+        return mensajes[0];
+      case 1:
+        return mensajes[1];
+      case 2:
+        return mensajes[2];
+      case 3:
+        return mensajes[3];
+      case 4:
+        return mensajes[4];
+      case 5:
+        return mensajes[5];
+      case 6:
+        return mensajes[6];
+      default:
+        return "Error";
+    }
+  })();
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="w-full h-screen flex justify-center items-center">
+        <Card className="max-w-[400px]">
+          <CardHeader className="flex gap-3">
+            <div className="w-full flex flex-col items-center">
+              <p className="text-md">{fechaActual}</p>
+            </div>
+          </CardHeader>
+          <Divider />
+          <CardBody>
+            <p>{message}</p>
+          </CardBody>
+          <Divider />
+        </Card>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { NextUIProvider } from "@nextui-org/react";
+import { createHashRouter, RouterProvider } from "react-router-dom";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const Router = createHashRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={Router} />
+    <NextUIProvider>
+      <BrowserRouter></BrowserRouter>
+    </NextUIProvider>
+  </React.StrictMode>
+);
