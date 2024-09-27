424252fb831abbb5581b1c8956dcf4e6dab18750
MrBowis <02alejo20@gmail.com>
2024-09-27 16:35:25 -0500

    Fix deploy error

--- src/main.tsx
+++ src/main.tsx
@@ -13,12 +13,17 @@ const Router = createHashRouter([
   },
 ]);
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <RouterProvider router={Router} />
-    <NextUIProvider>
-      <BrowserRouter></BrowserRouter>
-    </NextUIProvider>
-  </React.StrictMode>
-);
+const rootElement = document.getElementById("root");
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <RouterProvider router={Router} />
+      <NextUIProvider>
+        <BrowserRouter></BrowserRouter>
+      </NextUIProvider>
+    </React.StrictMode>
+  );
+} else {
+  console.error("Root element not found");
+}
